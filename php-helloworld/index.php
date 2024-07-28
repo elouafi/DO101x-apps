c45abb978a1345aba5e654ed82fb6505adfb9c80
EL OUAFI Omar <omar.elouafi@um6p.ma>
2024-07-28 18:05:38 +0100

    fix error

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming from Omar repo!\n"
-print "new change is comming to trigger the S2I process again and deploy the app \n"
+print "A change is coming from Omar repo!\n";
+print "new change is comming to trigger the S2I process again and deploy the app \n";
 ?>
