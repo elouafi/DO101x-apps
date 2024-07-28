d502d424c5cac9d00fb7f4db9dd22331bdc58d55
EL OUAFI Omar <omar.elouafi@um6p.ma>
2024-07-28 17:58:20 +0100

    new changes to php hw app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is coming from Omar repo!\n"
+print "new change is comming to trigger the S2I process again and deploy the app \n"
 ?>
